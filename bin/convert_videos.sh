4533941407ae1167137b5657f461f5d9928719e7
Brahim Hafah <b.hafah@hafah.nl>
2018-07-09 12:07:57 +0200

    adding convert videos script

---
+++ bin/convert_videos.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+# I sometimes record screencasts of me programming so I can critique my programming habits
+# these screencasts through OBS studio are in flv and this script is to convert them to mp4
+
+
+main() {
+    go_to_folder
+    convert_all
+}
+
+go_to_folder() {
+    cd ~/repos
+    cd ..
+    cd * #home folder in windows
+    cd Videos
+}
+
+convert_all() {
+    for i in *.flv 
+    do 
+        ffmpeg -i "$i" -codec copy "${i%.*}.mp4"
+    done
+}
+
+main
